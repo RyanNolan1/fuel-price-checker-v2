6e123ea912620de66cddc90a21f1c3b59f135f77
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-02 17:25:46 +0100

    this will add the initial fetch request

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 require("dotenv").config();
 
-console.log(process.env.API_KEY);
+const api_key = process.env.API_KEY;
+
+fetch(`https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=BB185AJ`)
+.then(response => response.json())
+.then(data => console.log(data.Response.DataItems.FuelStationDetails))
+.catch(error => console.log(error));
