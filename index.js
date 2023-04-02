8fe06c3bb31acc5968d3389e6cca394e3585e596
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-02 18:18:29 +0100

    this will add the code to sort the stations by distance

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ require("dotenv").config();
 
 const api_key = process.env.API_KEY;
 
+
 fetch(`https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=BB185AJ`)
 .then(response => response.json())
-.then(data => console.log(data.Response.DataItems.FuelStationDetails))
+.then(data => {
+    const stationData = data.Response.DataItems.FuelStationDetails.FuelStationList
+    sortByDistance(stationData)
+})
 .catch(error => console.log(error));
+
+
+
+function sortByDistance(stationData) {
+    const sortedByDistance = stationData.sort((a, b) => {
+        return a.DistanceFromSearchPostcode - b.DistanceFromSearchPostcode
+    });
+    console.log(sortedByDistance)
+}
