a4ca64a15a9ca2e110136896649ca67716c9c494
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-02 19:13:10 +0100

    this will add the initial code for the sort by price function

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const api_key = process.env.API_KEY;
+const api_key = "";
+//  process.env.API_KEY;
 
-
-fetch(`https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=BB185AJ`)
-.then(response => response.json())
-.then(data => {
-    const stationData = data.Response.DataItems.FuelStationDetails.FuelStationList
-    sortByDistance(stationData)
-})
-.catch(error => console.log(error));
-
-
+fetch(
+  `https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=BB185AJ`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const stationData =
+      data.Response.DataItems.FuelStationDetails.FuelStationList;
+    sortByDistance(stationData);
+    sortByPrice(stationData);
+  })
+  .catch((error) => console.log(error));
 
 function sortByDistance(stationData) {
-    const sortedByDistance = stationData.sort((a, b) => {
-        return a.DistanceFromSearchPostcode - b.DistanceFromSearchPostcode
-    });
-    console.log(sortedByDistance)
+  const sortedByDistance = stationData.sort((a, b) => {
+    return a.DistanceFromSearchPostcode - b.DistanceFromSearchPostcode;
+  });
+  // console.log(sortedByDistance)
 }
+
+const selectedFuel = "Diesel";
+
+let sortedByPriceArray = [];
+
+function sortByPrice(stationData) {
+  stationData.forEach(function (station) {
+    const fuelTypes = station.FuelPriceList;
+    // console.log(station.FuelPriceList)
+
+    fuelTypes.forEach(function (fuel) {
+      if (fuel.FuelType === selectedFuel) {
+        console.log(
+          station.DistanceFromSearchPostcode,
+          station.Brand,
+          station.Name,
+          station.Street,
+          station.Town,
+          station.County,
+          station.Postcode,
+          fuel.FuelType,
+          fuel.LatestRecordedPrice.InPence
+        );
+      }
+    });
+  });
+}
+
+//     station.FuelPriceList.forEach(function (fuel) {
+//       if (fuelType === fuel.FuelType) {
+//         priceArray.push(station);
+//       }
+//     });
+//   });
+//   const sortedByPrice = priceArray.sort((a, b) => {
+//     return a.LatestRecordedPrice.InPence - b.LatestRecordedPrice.InPence;
+//   });
+//   console.log(sortedByPrice);
