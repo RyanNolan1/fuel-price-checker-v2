f5b6269fc2359ce3efb11db0ea458185c0097eda
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-14 16:50:50 +0100

    this will make it so that the fuel buttons filter the data by fuel type

--- index.js
+++ index.js
@@ -4,44 +4,61 @@ const api_key = "";
 
 // process.env.API_KEY;
 
-fetch(
-  `https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=BB185AJ`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const stationData =
-      data.Response.DataItems.FuelStationDetails.FuelStationList;
-    sortByPrice(stationData);
-  })
-  .catch((error) => console.log(error));
+document.addEventListener("DOMContentLoaded", () => {
+  const postCodeInput = document.getElementById("post-code-input");
+  const postCodeSubmit = document.getElementById("post-code-submit");
+  let selectedFuel;
+  let stationData;
 
-let selectedFuel = "Unleaded";
+  postCodeSubmit.addEventListener("click", () => {
+    postCode = postCodeInput.value;
 
-let newFuelArray = [];
-
-function sortByPrice(stationData) {
-  stationData.forEach((station) => {
-    station.FuelPriceList.forEach((fuel) => {
-      if (fuel.FuelType === selectedFuel)
-        newFuelArray.push({
-          Brand: station.Brand,
-          County: station.County,
-          DistanceFromSearchPostcode: station.DistanceFromSearchPostcode,
-          Name: station.Name,
-          Street: station.Street,
-          Suburb: station.Suburb,
-          FuelPrice: fuel.LatestRecordedPrice.InPence,
-          FuelType: fuel.FuelType,
-        });
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        selectedFuel = this.value;
+        sortByPrice(selectedFuel, stationData);
+      });
     });
-  });
-  newFuelArray.sort((a, b) => a.FuelPrice - b.FuelPrice);
-  displayStations(newFuelArray);
-}
 
-function displayStations(data) {
-  const stations = newFuelArray.map(
-    (station) => `
+    fetch(
+      `https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=${postCode}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        stationData =
+          data.Response.DataItems.FuelStationDetails.FuelStationList;
+        sortByPrice(selectedFuel, stationData);
+      })
+      .catch((error) => console.log(error));
+  });
+
+  let newFuelArray = [];
+
+  function sortByPrice(selectedFuel, stationData) {
+    console.log(selectedFuel);
+    stationData.forEach((station) => {
+      station.FuelPriceList.forEach((fuel) => {
+        if (fuel.FuelType === selectedFuel)
+          newFuelArray.push({
+            Brand: station.Brand,
+            County: station.County,
+            DistanceFromSearchPostcode: station.DistanceFromSearchPostcode,
+            Name: station.Name,
+            Street: station.Street,
+            Suburb: station.Suburb,
+            FuelPrice: fuel.LatestRecordedPrice.InPence,
+            FuelType: fuel.FuelType,
+          });
+      });
+    });
+    newFuelArray.sort((a, b) => a.FuelPrice - b.FuelPrice);
+    displayStations(newFuelArray);
+  }
+
+  function displayStations() {
+    const stations = newFuelArray.map(
+      (station) => `
   <li>
   ${station.Brand}
   ${station.County}
@@ -50,11 +67,12 @@ function displayStations(data) {
   ${station.Street}
   ${station.FuelPrice}
   </li>`
-  );
+    );
 
-  const stationList = document.querySelector("#station-list");
-  stationList.innerHTML = stations.join("");
-}
+    const stationList = document.querySelector("#station-list");
+    stationList.innerHTML = stations.join("");
+  }
+});
 
 // function sortByDistance(arr) {
 //   arr.sort((a, b) => {
